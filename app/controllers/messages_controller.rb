fbb7dd9e5d8198fe7897739130389b1f0e85ac88
corlassion <corlassion@gmail.com>
2015-02-09 14:49:21 -0800

    initial prototype for deliverable

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,47 @@
+class MessagesController < ApplicationController
+
+	def index
+		@start_time = (Date.today - 5).to_s(:db)
+		@end_time = Date.today.to_s(:db)
+		@items_count = 5
+	end
+
+	def create
+		@view_messages = []
+		build_array()
+		#binding.pry
+		if !@view_messages.any?
+			render "norecord"
+		end
+	end
+
+	def build_array()
+		@view_messages = []
+		@start_time = (Date.today - 5).to_s(:db)
+		@end_time = (Date.today + 1).to_s(:db)
+		@items_count = 5
+		cmd = "SELECT * FROM view_messages(\'#{@start_time}\', \'#{@end_time}\', \' #{@items_count}\')" 
+		temp_results = ActiveRecord::Base.connection.execute(cmd)
+		#binding.pry
+		temp_results.each do |view_message_temp|
+			view_message = ViewMessage.new( view_message_temp["code_id"],
+											view_message_temp["message_id"],
+											view_message_temp["message"],
+											view_message_temp["username"],
+											view_message_temp["usertype"],
+											view_message_temp["user_id"],
+											view_message_temp["location_name"],
+											view_message_temp["code_type"],
+											view_message_temp["code_action"],
+											view_message_temp["message_created_at"],
+											)
+			if view_message.username == "SMichal"
+				@view_messages << view_message
+			end
+			#binding.pry
+		end
+	end
+
+
+
+end
---
+++ app/models/view_message.rb
@@ -0,0 +1,36 @@
+class ViewMessage
+	attr_accessor :code_id
+	attr_accessor :message_id
+	attr_accessor :message
+	attr_accessor :username
+	attr_accessor :usertype
+	attr_accessor :user_id
+	attr_accessor :location_name
+	attr_accessor :code_type
+	attr_accessor :code_action
+	attr_accessor :message_created_at
+
+	def initialize( code_id,
+					message_id,
+					message,
+					username,
+					usertype,
+					user_id,
+					location_name,
+					code_type,
+					code_action,
+					message_created_at)
+		@code_id = code_id
+		@message_id = message_id
+		@message = message
+		@username = username
+		@usertype = usertype
+		@user_id = user_id
+		@location_name = location_name
+		@code_type = code_type
+		@code_action = code_action
+		@message_created_at = message_created_at
+
+	end
+
+end
